86c8df7f66484ceba961e9785da11895b7211cc4
Roberto Ruiz <rruiz@algeiba.com>
2021-03-20 19:01:43 -0300

    Se agregan proyectos de tests

---
+++ backend/src/AVI.Clima.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AVI.Clima.Infrastructure.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
